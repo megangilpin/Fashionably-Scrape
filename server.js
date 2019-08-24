7a730c407f84b78efb0d03a8eaafe19d4d4c6111
megangilpin <48898520+megangilpin@users.noreply.github.com>
2019-08-24 18:23:20 -0400

    added mongoose server info

--- server.js
+++ server.js
@@ -27,7 +27,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/fashionablyscraperdb", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/fashionablyscraperdb", { useNewUrlParser: true });
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
